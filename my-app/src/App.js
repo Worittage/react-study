c78236f652460bff9a97cf0067dc6466ad72c876
Worittage <122890375+Worittage@users.noreply.github.com>
2023-06-13 00:04:22 +0200

    6 Lesson

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="parrents">
+      <div></div>
+      <div className="App">
+        <ul>
+          <li>css</li>
+          <li>html</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
