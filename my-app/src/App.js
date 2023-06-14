6a26c619d2f356f6734d3fda3ac7b59c02ce76d0
Worittage <122890375+Worittage@users.noreply.github.com>
2023-06-14 23:39:39 +0200

    lesson 07

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,33 @@ import "./App.css";
 function App() {
   return (
     <div className="parrents">
-      <div></div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-        </ul>
-      </div>
+      <Header />
+      <Technologies />
     </div>
   );
 }
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">News Feed</a>
+      <a href="#s">Message</a>
+    </div>
+  );
+};
 
 export default App;
